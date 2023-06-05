76bf79b89ee064cf817db3588a3e90e2eeab731b
Mush <Muaz@outlook.dk>
2023-06-05 12:41:15 +0200

    Added open and close port on robot

--- ball_collector/main.py
+++ ball_collector/main.py
@@ -7,7 +7,6 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.o
 # Click "Open user guide" on the EV3 extension tab for more information.
 
@@ -15,7 +14,7 @@ import socket
 import time
 # Set the IP address and port number for the server
 HOST = '172.20.10.13'  # Replace with the IP address of your computer
-PORT = 12345
+PORT = 1234
 
 # Create a new socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,26 +53,39 @@ while True:
     
     # Parse the command and set the motor speeds
     if command == 'forward':
-        robot.drive(500, 0)
+        robot.drive(200, 0)
         left_arm = Motor(Port.C, Direction.COUNTERCLOCKWISE, [12, 36])
-        left_arm.control.limits(speed=60, acceleration=120)
-        left_arm.run(60)
+        left_arm.control.limits(speed=100, acceleration=120)
+        left_arm.run(100)
+        if ultra.distance() < 400:
+            print("ultra: ", ultra.distance())
+            print("Turning inside if")
+            s.sendall('Turning'.encode())
+            wait(3)
+            robot.turn(200)
         print("ultra: ", ultra.distance())
     elif command == 'backward':
         left_arm.run(0)
-        robot.drive(-500, 0)
+        robot.drive(-200, 0)
     elif command == 'left':
         robot.drive(0, 200)
     elif command == 'right':
         robot.drive(0, -200)
     elif command == 'stop':
         robot.stop()
-
-    while ultra.distance() < 300:                   #kode delen for at robotten kan køre plus se om der er en væg ( den som var vist på video)
+    elif command == "open":
+        port = Motor(Port.A, Direction.CLOCKWISE, [12, 36])
+        port.control.limits(speed=60, acceleration=120)
+        port.run(60)
+    elif command == "close":
+        port = Motor(Port.A, Direction.COUNTERCLOCKWISE, [12, 36])
+        port.control.limits(speed=60, acceleration=120)
+        port.run(60)
+    while ultra.distance() < 400:                   #kode delen for at robotten kan køre plus se om der er en væg ( den som var vist på video)
         print("Turning")
         s.sendall('Turning'.encode())
-        wait(10)
-        robot.turn(500)
+        wait(4)
+        robot.turn(250)
 
 # Clean up
 robot.stop()
