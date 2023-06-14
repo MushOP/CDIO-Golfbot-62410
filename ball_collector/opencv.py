154333972d9bf7db0cdcf746d86f4dbc812368a6
Mush <Muaz@outlook.dk>
2023-06-14 15:46:00 +0200

    Ball is now only detected inside field/border

--- ball_collector/opencv.py
+++ ball_collector/opencv.py
@@ -112,7 +112,7 @@ def detect_pink(frame):
         center = np.mean(box, axis=0)
         return center
         
-def detect_white_balls(frame):
+def detect_white_balls(frame, polygon_pts):
     # Convert the frame to the HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -139,23 +139,24 @@ def detect_white_balls(frame):
         area = cv2.contourArea(contour)
         perimeter = cv2.arcLength(contour, True)
         circularity = 4 * np.pi * (area / (perimeter * perimeter))
-        if min_area < area < max_area and 0.5 < circularity < 1.2:  # adjust circularity range as needed
+        # Check if the ball center is inside the polygon
+        if (min_area < area < max_area and 0.5 < circularity < 1.2):  # adjust circularity range as needed
             valid_contours.append(contour)
 
     # Draw bounding circles around valid contours
-    for contour in valid_contours:
-        (x, y), radius = cv2.minEnclosingCircle(contour)
-        center = (int(x), int(y))
-        radius = int(radius)
-        cv2.circle(frame, center, radius, (0, 255, 255), 2)
-
     # Extract valid contour coordinates and centers
     coordinates = []
     centers = []
     for contour in valid_contours:
         (x, y), radius = cv2.minEnclosingCircle(contour)
-        coordinates.append((int(x - radius), int(y - radius), int(radius * 2), int(radius * 2)))
-        centers.append((int(x), int(y)))
+        center = (int(x), int(y))
+        radius = int(radius)
+        #cv2.circle(frame, center, radius, (0, 255, 255), 2)
+        # Check if the ball center is inside the polygon
+        if cv2.pointPolygonTest(np.array(polygon_pts), center, False) >= 0:
+            cv2.circle(frame, center, radius, (0, 255, 255), 2)
+            coordinates.append((int(x - radius), int(y - radius), int(radius * 2), int(radius * 2)))
+            centers.append(center)
 
     # Create a graph with valid contour coordinates as vertices
     graph = {coord: {} for coord in coordinates}
@@ -172,34 +173,47 @@ def detect_white_balls(frame):
     return frame, centers, graph
 
 def detect_red(frame):
+    # Convert the frame to HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Define the lower and upper red color ranges in HSV
     hsv_values = {'hmin': 0, 'smin': 170, 'vmin': 175, 'hmax': 10, 'smax': 255, 'vmax': 255}
-    """
-    lower_red1 = np.array([0, 70, 50])
-    upper_red1 = np.array([10, 255, 255])
-    lower_red2 = np.array([170, 70, 50])
-    upper_red2 = np.array([180, 255, 255])
-    """
     lower_red = np.array([hsv_values['hmin'], hsv_values['smin'], hsv_values['vmin']])
     upper_red = np.array([hsv_values['hmax'], hsv_values['smax'], hsv_values['vmax']])
+    
     # Threshold the HSV image to get only the red color
-    #mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-    #mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-
-    # Combine the masks
-    #mask = cv2.bitwise_or(mask1, mask2)
     mask = cv2.inRange(hsv, lower_red, upper_red)
+    
     # Apply morphological operations to remove noise from the mask
     kernel = np.ones((5, 5), np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)  # Changed to MORPH_CLOSE
-
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    
     # Find contours of the red regions
-    contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)  # Changed to RETR_LIST
+    contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Loop through the contours
+    for contour in contours:
+        # Check contour size, if it's too small, skip it
+        if cv2.contourArea(contour) < 100:
+            continue
 
-    # Draw contours on the frame
-    cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        # Find the minimum enclosing circle for each contour
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        center = (int(x), int(y))
+        radius = int(radius)
+        
+        # Calculate the coordinates of each corner of a square circumscribing the circle
+        half_side_length = radius * np.sqrt(2)
+        top_left = (int(x - half_side_length), int(y - half_side_length))
+        top_right = (int(x + half_side_length), int(y - half_side_length))
+        bottom_left = (int(x - half_side_length), int(y + half_side_length))
+        bottom_right = (int(x + half_side_length), int(y + half_side_length))
+        
+        # Draw a small circle at each corner of the square
+        cv2.circle(frame, top_left, 5, (0, 255, 255), -1)
+        cv2.circle(frame, top_right, 5, (0, 255, 255), -1)
+        cv2.circle(frame, bottom_left, 5, (0, 255, 255), -1)
+        cv2.circle(frame, bottom_right, 5, (0, 255, 255), -1)
 
     return frame
 
@@ -283,21 +297,159 @@ def white_balance(frame):
     result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
     return result
 
+
+manMode = False
+selectCount = 0
+# Field dimensions
+width = 750
+height = 500
+corners = [[0, 0], [750, 0], [0, 500], [750, 500]]
+goals = [[0, 250], [750, 250]]
+
+def transform(frame, frameCount):
+    if (not manMode and frameCount % 20 == 0):
+        corners = getCorners(frame)
+        goals = getGoals(corners)
+
+    if (corners != None):
+        # Coordinates for the corners
+        oldCoordinates = np.float32([corners[0], corners[1],
+                                        corners[2], corners[3]])
+
+        # New coordinates for the corners
+        newCoordinates = np.float32([[0, 0], [width, 0],
+                                        [0, height], [width, height]])
+
+        # Transform image
+        matrix = cv2.getPerspectiveTransform(
+            oldCoordinates, newCoordinates)
+        transformed = cv2.warpPerspective(
+            frame, matrix, (width, height))
+        # Create circles to indicate detected corners and goals
+        frame = cv2.circle(frame, tuple(
+            corners[0]), 20, (255, 0, 0), 2)
+        frame = cv2.circle(frame, tuple(
+            corners[1]), 20, (255, 0, 0), 2)
+        frame = cv2.circle(frame, tuple(
+            corners[2]), 20, (255, 0, 0), 2)
+        frame = cv2.circle(frame, tuple(
+            corners[3]), 20, (255, 0, 0), 2)
+        frame = cv2.circle(frame, tuple(goals[0]), 20, (255, 0, 0), 2)
+        frame = cv2.circle(frame, tuple(goals[1]), 20, (255, 0, 0), 2)
+        return transformed
+import numpy as np
+import cv2
+
+def getcorners(frame):
+    # Define lower/upper color field for red
+    lower_red = np.array([0, 0, 200], dtype="uint8")
+    upper_red = np.array([100, 100, 255], dtype="uint8")
+    
+    # Frame dimensions
+    height, width = frame.shape[:2]
+
+    # Create a mask for red color
+    red_mask = cv2.inRange(frame, lower_red, upper_red)
+
+    # Filter out everything that is not red
+    wall = cv2.bitwise_and(frame, frame, mask=red_mask)
+    
+    # Convert to grayscale
+    wall = cv2.cvtColor(wall, cv2.COLOR_BGR2GRAY)
+
+    # Find corners
+    dest = cv2.cornerHarris(np.float32(wall), 20, 9, 0.14)
+    dest = cv2.dilate(dest, None)
+
+    # Threshold for detecting corners
+    thresh = 0.01 * dest.max()
+    
+    # Get the coordinates of the corners
+    corners_y, corners_x = np.where(dest > thresh)
+
+    # Calculate center
+    center_x, center_y = width / 2, height / 2
+    
+    # Initialize corner coordinates
+    upper_left = [width, height]
+    upper_right = [0, height]
+    lower_left = [width, 0]
+    lower_right = [0, 0]
+
+    # Iterate through the detected corners and update the corner coordinates
+    for x, y in zip(corners_x, corners_y):
+        if x < center_x and y < center_y:
+            upper_left = [min(upper_left[0], x), min(upper_left[1], y)]
+        elif x > center_x and y < center_y:
+            upper_right = [max(upper_right[0], x), min(upper_right[1], y)]
+        elif x < center_x and y > center_y:
+            lower_left = [min(lower_left[0], x), max(lower_left[1], y)]
+        else:
+            lower_right = [max(lower_right[0], x), max(lower_right[1], y)]
+            
+    # Draw circles at the corners
+    cv2.circle(frame, tuple(map(int, upper_left)), 20, (255, 0, 0), 2)
+    cv2.circle(frame, tuple(map(int, upper_right)), 20, (255, 0, 0), 2)
+    cv2.circle(frame, tuple(map(int, lower_left)), 20, (255, 0, 0), 2)
+    cv2.circle(frame, tuple(map(int, lower_right)), 20, (255, 0, 0), 2)
+
+    # Return the coordinates of the corners
+    return [upper_left, upper_right, lower_left, lower_right]
+
+
+def getGoals(corners):
+    goal_left = [0, 99999]
+    goal_right = [99999, 0]
+    upper_left = corners[0]
+    upper_right = corners[1]
+    lower_left = corners[2]
+    lower_right = corners[3]
+
+    goal_left[0] = int((upper_left[0] + lower_left[0]) / 2)
+    goal_left[1] = int((upper_left[1] + lower_left[1]) / 2)
+    goal_right[0] = int((upper_right[0] + lower_right[0]) / 2)
+    goal_right[1] = int((upper_right[1] + lower_right[1]) / 2)
+
+    return [goal_left, goal_right]
+    """
+Coordinates at click: 1663, 183
+Coordinates at click: 1688, 1039
+Coordinates at click: 450, 1019
+Coordinates at click: 526, 134"""
+
+def draw_circles_at_coordinates(frame):
+    # Define a list of hardcoded coordinates
+    hardcoded_coordinates = [(1663, 183), (1688, 1039), (450, 1019), (526, 134)]
+    
+    # Get frame dimensions
+    height, width = frame.shape[:2]
+    
+    # Convert hardcoded coordinates to a numpy array
+    polygon_pts = np.array(hardcoded_coordinates, dtype=np.int32)
+    polygon_pts = polygon_pts.reshape((-1, 1, 2))
+    
+    # Draw the border (polygon) by connecting coordinates
+    cv2.polylines(frame, [polygon_pts], isClosed=True, color=(255, 0, 0), thickness=2)
+
+    return polygon_pts  # Return the polygon points for later use
+
 # Loop over frames from the video stream
 while True:
     # Read a frame from the video stream
     ret, frame = cap.read()
-    frame = white_balance(frame)
+    #frame = white_balance(frame)
+    # Draw circles and get the polygon points
+    polygon_pts = draw_circles_at_coordinates(frame)
 
     # Detect the green rectangle (robot)
     robot = detect_robot(frame)
 
     # Detect the white balls and get valid contour coordinates and graph
-    frame, ball_centers, graph = detect_white_balls(frame)
+    frame, ball_centers, graph = detect_white_balls(frame, polygon_pts)
 
     pink = detect_pink(frame)
 
-    red = detect_red(frame)
+    #red = detect_red(frame)
     
     # Find the starting point on the table tennis table
     start = robot[:2] if robot is not None else None
@@ -321,10 +473,16 @@ while True:
         # Mark the left edge as the goal (draw a circle)
         cv2.circle(frame, left_edge, 5, (0, 255, 255), -1)
 
+        # Filter ball_centers - keep only those that are inside the polygon
+        inside_ball_centers = [pt for pt in ball_centers if cv2.pointPolygonTest(polygon_pts, pt, False) >= 0]
+        
+        # Find the shortest path to the closest ball inside the polygon using Dijkstra's algorithm
+        closest_ball = closest_node(start, inside_ball_centers) if inside_ball_centers else None
+
         # Find the shortest path to the closest ball using Dijkstra's algorithm
         current_time = time.time()
         #if current_time - last_update_time >= 4:
-        closest_ball = closest_node(start, ball_centers)
+        #closest_ball = closest_node(start, ball_centers)
             #last_update_time = current_time
             #closest_ball = closest_node(start, ball_centers)
         robo_angle = 0
