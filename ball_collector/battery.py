516763b696355ad1b86962fa41becc46c2f68d1a
Klakawaka <91070430+Klakawaka@users.noreply.github.com>
2023-06-22 21:18:50 +0200

    commiting the  battery.py program

--- ball_collector/battery.py
+++ ball_collector/battery.py
@@ -1,1 +1,13 @@
-# Check current battery on EV3 robot
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+import time
+
+ev3 = EV3Brick()
+
+while True:
+    battery_voltage = ev3.battery.voltage()
+    battery_percentage = (battery_voltage / 9000) * 100  # Assuming 9000mV is fully charged
+    print("Battery Level: {:.2f}%".format(battery_percentage))
+    time.sleep(1)  # Wait for 1 second before checking again
+
+# Check the current battery on your EV3 Brick
